cde08700a4d9479c28dad1d6a783b9678e187bf0
Stanislav Petrov <petrovst@mail.ru>
2023-11-29 17:08:05 +0300

    изменил лабиринт, добавил очки, перенес функции

--- pacman_aliexpress.py
+++ pacman_aliexpress.py
@@ -18,8 +18,16 @@ class Wall(GameObject):
 class Chest(GameObject):
     sprite_filename = "chest"
 
+LEFT_X = 40
+RIGHT_X = 560
+HIGHT_Y = 40
+LOW_Y = 400
+
+HEALTH = 10
+
 
 def calculate_walls_coordinates(screen_width, screen_height, wall_block_width, wall_block_height):
+    """функция вырисовки стен по периметру"""
     horizontal_wall_blocks_amount = screen_width // wall_block_width
     vertical_wall_blocks_amount = screen_height // wall_block_height - 2
 
@@ -37,58 +45,103 @@ def calculate_walls_coordinates(screen_width, screen_height, wall_block_width, w
 
     return walls_coordinates
 
-# тут добавил рандомный лабиринт
+
 def calculate_lab_coordinates(screen_width, screen_height, wall_block_width, wall_block_height):
+    """функция вырисовки лабиринта"""
     horizontal_wall_blocks_amount = screen_width // wall_block_width
-    vertical_wall_blocks_amount = screen_height // wall_block_height - 2
+    vertical_wall_blocks_amount = screen_height // wall_block_height
 
     lab_coordinates = []
-    random_lab = random.randint(2,4)
-    for block_num in range(2,horizontal_wall_blocks_amount):
-        if block_num%random_lab!=0:
+
+    random_hole = random.randint(4,5)
+    random_lab = 3
+    for block_num in range(2,horizontal_wall_blocks_amount -2):
+        if block_num%random_hole!=0:
             lab_coordinates.extend([
-                (block_num * wall_block_width, wall_block_height*3),
-                (block_num * wall_block_width, wall_block_height*6),
-                (block_num * wall_block_width, wall_block_height*9),
+                (block_num * wall_block_width, wall_block_height*random_lab),
+                (block_num * wall_block_width, wall_block_height*random_lab*2),
+                (block_num * wall_block_width, wall_block_height*random_lab*3),
             ])
-    for block_num in range(2, vertical_wall_blocks_amount + random_lab):
-        if block_num%random_lab!=0:
+    for block_num in range(2, vertical_wall_blocks_amount - 2):
+        if block_num%random_hole!=0:
             lab_coordinates.extend([
-                (wall_block_width*3, block_num * wall_block_height),
-                (wall_block_width*6, block_num * wall_block_height),
-                (wall_block_width*9, block_num * wall_block_height),
-                (wall_block_width*12, block_num * wall_block_height)
+                (wall_block_width*random_lab, block_num * wall_block_height),
+                (wall_block_width*random_lab*2, block_num * wall_block_height),
+                (wall_block_width*random_lab*3, block_num * wall_block_height),
+                (wall_block_width*random_lab*4, block_num * wall_block_height)
             ])
 
     return lab_coordinates
 
 
 def compose_context(screen):
+    """функция обЪявления обЪектов"""
     walls_coordinates = calculate_walls_coordinates(screen.get_width(), screen.get_height(), Wall.width, Wall.height)
-    lab_coordinates = calculate_lab_coordinates(screen.get_width() - 160, screen.get_height() - 160, Wall.width, Wall.height)
+    lab_coordinates = calculate_lab_coordinates(screen.get_width(), screen.get_height(), Wall.width, Wall.height)
     all_coordinates = walls_coordinates + lab_coordinates
-    # all_coordinates = walls_coordinates
     return {
-        "player": Player(40, 400),
+        "player": Player(LEFT_X, LOW_Y),
         "walls": Group(*[Wall(x, y) for (x, y) in all_coordinates]),
-        "score": 10,
-        "chest": Chest(40, 40),
-        "chest_2": Chest(560, 40),
-        # "chest_3": Chest(100, 340),
-        # "chest_4": Chest(500, 340),
+        "score_name": "health: ",
+        "score": HEALTH,
+        "score_2_name": "points: ",
+        "score_2": 0,
+        "chest": Chest(LEFT_X, HIGHT_Y),
+        "chest_2": Chest(RIGHT_X, HIGHT_Y),
     }
 
 
 def draw_whole_screen(screen, context):
+    """функция вырисовки обЪектов"""
     screen.fill("purple")
     context["player"].draw(screen)
     context["walls"].draw(screen)
     context["chest"].draw(screen)
     context["chest_2"].draw(screen)
-    # context["chest_3"].draw(screen)
-    # context["chest_4"].draw(screen)
-    Text(str(context["score"]), (10, 10)).draw(screen)
+    Text(str(context["score_name"]), (10, 10)).draw(screen)
+    Text(str(context["score"]), (90, 10)).draw(screen)
+    Text(str(context["score_2_name"]), (130, 10)).draw(screen)
+    Text(str(context["score_2"]), (210, 10)).draw(screen)
 
+
+def move_chest(chest, player):
+    """функция приближения ящика к игроку"""
+    chest_speed = random.randint(1,7)
+    old_chest_x, old_chest_y, width_chest, height_chest = chest.rect
+    old_player_x, old_player_y, width_player, height_player = player.rect
+    
+    if old_player_x > old_chest_x:
+        step_x = chest.rect.move(chest_speed, 0)
+    elif old_player_x < old_chest_x:
+        step_x = chest.rect.move(-1 * chest_speed, 0)
+    else:
+        step_x = chest.rect.move(0, 0)
+    
+    if old_player_y > old_chest_y:
+        step_y = chest.rect.move(0, chest_speed)
+    elif old_player_y < old_chest_y:
+        step_y = chest.rect.move(0, -1 * chest_speed)
+    else:
+        step_y = chest.rect.move(0, 0)
+
+    chest.rect = random.choice([step_x, step_y])
+
+
+def move_player(context):
+    """функция движения игрока клавишами"""
+    player_speed = 5
+    keys = pygame.key.get_pressed()
+
+    pressed_dict = {
+        pygame.K_w: (0, -1 * player_speed),
+        pygame.K_s: (0, player_speed),
+        pygame.K_a: (-1 * player_speed, 0),
+        pygame.K_d: (player_speed, 0)
+    }
+
+    for k,v in pressed_dict.items():
+        if keys[k]:
+            context["player"].rect = context["player"].rect.move(v[0], v[1])
 
 
 def main():
@@ -96,85 +149,61 @@ def main():
     screen = pygame.display.set_mode((640, 480))
     clock = pygame.time.Clock()
     running = True
-    player_speed = 5
-
+    
+    # обЪявляем обЪекты
     context = compose_context(screen)
 
+    # кнопка выключения
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        # включаем вырисовку
         draw_whole_screen(screen, context)
         pygame.display.flip()
 
-        keys = pygame.key.get_pressed()
-
+        # сохраним старый ход игрока и будем двигать игрока
         old_player_topleft = context["player"].rect.topleft
-        if keys[pygame.K_w]:
-            context["player"].rect = context["player"].rect.move(0, -1 * player_speed)
-        if keys[pygame.K_s]:
-            context["player"].rect = context["player"].rect.move(0, player_speed)
-        if keys[pygame.K_a]:
-            context["player"].rect = context["player"].rect.move(-1 * player_speed, 0)
-        if keys[pygame.K_d]:
-            context["player"].rect = context["player"].rect.move(player_speed, 0)
+        move_player(context)
 
+        # сохраним старый ход ящиков
         old_chest_topleft = context["chest"].rect.topleft
         old_chest_2_topleft = context["chest_2"].rect.topleft
-        # вот тут попробую подвигать ящик
-        def move_chest(chest):
-            # chest_speed = 1
-            chest_speed = random.randint(1,7)
-            old_chest_x, old_chest_y, width_chest, height_chest = chest.rect
-            old_player_x, old_player_y, width_player, height_player = context["player"].rect
-            
-            if old_player_x > old_chest_x:
-                step_x = chest.rect.move(chest_speed, 0)
-            elif old_player_x < old_chest_x:
-                step_x = chest.rect.move(-1 * chest_speed, 0)
-            else:
-                step_x = chest.rect.move(0, 0)
-            
-            if old_player_y > old_chest_y:
-                step_y = chest.rect.move(0, chest_speed)
-            elif old_player_y < old_chest_y:
-                step_y = chest.rect.move(0, -1 * chest_speed)
-            else:
-                step_y = chest.rect.move(0, 0)
-
-            chest.rect = random.choice([step_x, step_y])
         
+        # двигаем ящики
         for chest in (context["chest"], context["chest_2"]):
-            move_chest(chest)
-        # алгоритм движения ящика закончен
+            move_chest(chest, context["player"])
 
-
-        if spritecollide(context["player"], context["walls"], dokill=False):
-            context["player"].rect.topleft = old_player_topleft
-
-        if spritecollide(context["chest"], context["walls"], dokill=False):
-            context["chest"].rect.topleft = old_chest_topleft
-
-        if spritecollide(context["chest_2"], context["walls"], dokill=False):
-            context["chest_2"].rect.topleft = old_chest_2_topleft
-            
+        # проверяем столкновение обЪектов со стеной
+        for obj in (
+            (context["player"], old_player_topleft),
+            (context["chest"], old_chest_topleft),
+            (context["chest_2"], old_chest_2_topleft)
+            ):
+            if spritecollide(obj[0], context["walls"], dokill=False):
+                obj[0].rect.topleft = obj[1]
+        
+        # проверяем столкновение игрока с ящиками
         for chest in (context["chest"], context["chest_2"]):
             if context["player"].is_collided_with(chest):
                 context["score"] -= 1
                 chest.rect.topleft = random.choice(
-                    [(40, 40), (560, 40), (40, 400)]
+                    [(LEFT_X, HIGHT_Y), (RIGHT_X, HIGHT_Y), (LEFT_X, LOW_Y)]
                     )
-        
-        if context["chest"].is_collided_with(context["chest_2"]):
-                context["chest"].rect.topleft = random.choice(
-                    [(40, 40), (560, 40), (40, 400)]
-                    )
-
+                
+        # если здоровье закончилось, конец игры
         if context["score"] == 0:
-
             running = False
+        
+        # проверяем столкновение ящиков между собой
+        if context["chest"].is_collided_with(context["chest_2"]):
+                context["score_2"] += 1
+                context["chest"].rect.topleft = random.choice(
+                    [(LEFT_X, HIGHT_Y), (RIGHT_X, HIGHT_Y), (LEFT_X, LOW_Y)]
+                    )
 
+        
         clock.tick(60) / 1000
 
     pygame.quit()
