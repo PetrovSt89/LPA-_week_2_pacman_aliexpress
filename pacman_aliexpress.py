d0b590ab8c8f88141639ebf8c916a30a470bc438
Stanislav Petrov <petrovst@mail.ru>
2023-11-30 11:46:01 +0300

    добавил лабиринт, поменял картинки, цвет, заставки

--- pacman_aliexpress.py
+++ pacman_aliexpress.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import pygame
 from pygame.sprite import Group, spritecollide
@@ -17,6 +18,14 @@ class Wall(GameObject):
 
 class Chest(GameObject):
     sprite_filename = "chest"
+
+
+class Chest_2(GameObject):
+    sprite_filename = "chest_2"
+
+class Chest_OLD(GameObject):
+    sprite_filename = "chest_old"
+
 
 LEFT_X = 40
 RIGHT_X = 560
@@ -53,7 +62,7 @@ def calculate_lab_coordinates(screen_width, screen_height, wall_block_width, wal
 
     lab_coordinates = []
 
-    random_hole = random.randint(4,5)
+    random_hole = 4
     random_lab = 3
     for block_num in range(2,horizontal_wall_blocks_amount -2):
         if block_num%random_hole!=0:
@@ -70,7 +79,7 @@ def calculate_lab_coordinates(screen_width, screen_height, wall_block_width, wal
                 (wall_block_width*random_lab*3, block_num * wall_block_height),
                 (wall_block_width*random_lab*4, block_num * wall_block_height)
             ])
-
+    
     return lab_coordinates
 
 
@@ -78,7 +87,12 @@ def compose_context(screen):
     """функция обЪявления обЪектов"""
     walls_coordinates = calculate_walls_coordinates(screen.get_width(), screen.get_height(), Wall.width, Wall.height)
     lab_coordinates = calculate_lab_coordinates(screen.get_width(), screen.get_height(), Wall.width, Wall.height)
-    all_coordinates = walls_coordinates + lab_coordinates
+    lab_1 = [(120,40), (80, 80), (80, 200), (80, 320), (80, 360),(160, 120), (160, 240), (160, 280),
+             (120, 360), (160, 360), (200, 80), (240, 160), (200, 200), (320, 40), (360, 40),(240, 200), (240, 240),
+             (280, 320), (320, 320), (240, 400), (360, 360), (440, 400), (560, 200), (320, 200), (520, 360),
+             (400, 120), (400, 160), (400, 280), (480, 80), (480, 240), (480, 280), (520, 80), (360, 120)]
+            
+    all_coordinates = walls_coordinates + random.choice([lab_1, lab_coordinates])
     return {
         "player": Player(LEFT_X, LOW_Y),
         "walls": Group(*[Wall(x, y) for (x, y) in all_coordinates]),
@@ -87,17 +101,20 @@ def compose_context(screen):
         "score_2_name": "points: ",
         "score_2": 0,
         "chest": Chest(LEFT_X, HIGHT_Y),
-        "chest_2": Chest(RIGHT_X, HIGHT_Y),
+        "chest_2": Chest_2(RIGHT_X, HIGHT_Y),
+        "chest_old": Chest_OLD(RIGHT_X, LOW_Y),
+        "all_coordinates": all_coordinates,
     }
 
 
 def draw_whole_screen(screen, context):
     """функция вырисовки обЪектов"""
-    screen.fill("purple")
+    screen.fill("grey")
     context["player"].draw(screen)
     context["walls"].draw(screen)
     context["chest"].draw(screen)
     context["chest_2"].draw(screen)
+    context["chest_old"].draw(screen)
     Text(str(context["score_name"]), (10, 10)).draw(screen)
     Text(str(context["score"]), (90, 10)).draw(screen)
     Text(str(context["score_2_name"]), (130, 10)).draw(screen)
@@ -127,6 +144,7 @@ def move_chest(chest, player):
     chest.rect = random.choice([step_x, step_y])
 
 
+
 def move_player(context):
     """функция движения игрока клавишами"""
     player_speed = 5
@@ -149,9 +167,17 @@ def main():
     screen = pygame.display.set_mode((640, 480))
     clock = pygame.time.Clock()
     running = True
+    lis_title = pygame.image.load("lis_title.png")
+    lis_game_over = pygame.image.load("resources/game_over.png")
     
+    screen.blit(pygame.transform.scale(lis_title, [640,480]), [10, 10])
+    pygame.display.flip()
+    time.sleep(2)
+
     # обЪявляем обЪекты
     context = compose_context(screen)
+
+
 
     # кнопка выключения
     while running:
@@ -170,16 +196,18 @@ def main():
         # сохраним старый ход ящиков
         old_chest_topleft = context["chest"].rect.topleft
         old_chest_2_topleft = context["chest_2"].rect.topleft
+
         
         # двигаем ящики
         for chest in (context["chest"], context["chest_2"]):
             move_chest(chest, context["player"])
 
+
         # проверяем столкновение обЪектов со стеной
         for obj in (
             (context["player"], old_player_topleft),
             (context["chest"], old_chest_topleft),
-            (context["chest_2"], old_chest_2_topleft)
+            (context["chest_2"], old_chest_2_topleft),
             ):
             if spritecollide(obj[0], context["walls"], dokill=False):
                 obj[0].rect.topleft = obj[1]
@@ -194,15 +222,28 @@ def main():
                 
         # если здоровье закончилось, конец игры
         if context["score"] == 0:
+            screen.blit(pygame.transform.scale(lis_game_over, [640,480]), [10, 10])
+            pygame.display.flip()
+            time.sleep(3)
             running = False
         
         # проверяем столкновение ящиков между собой
         if context["chest"].is_collided_with(context["chest_2"]):
-                context["score_2"] += 1
+                context["score"] += 1
                 context["chest"].rect.topleft = random.choice(
                     [(LEFT_X, HIGHT_Y), (RIGHT_X, HIGHT_Y), (LEFT_X, LOW_Y)]
                     )
 
+        if context["player"].is_collided_with(context["chest_old"]):
+            context["score_2"] += 1
+            while True:
+                position = Wall.width * random.randint(2, 13),Wall.height * random.randint(2, 9)
+                if position not in context["all_coordinates"]:
+                    context["chest_old"].rect.topleft = (
+                        position[0],
+                        position[1],
+                        )
+                    break
         
         clock.tick(60) / 1000
 
